6ae21732d475f7a2ae33b3af91b9f7bea4904e23
ayush2297 <ayush.saraf47@gmail.com>
2019-11-29 12:46:55 +0530

    added: writing data to csv file

--- src/main/java/com/opencsv/CsvUser.java
+++ src/main/java/com/opencsv/CsvUser.java
@@ -16,6 +16,16 @@ public class CsvUser {
     @CsvBindByName
     private String country;
 
+    public CsvUser() {
+    }
+
+    public CsvUser(String name, String email, String phoneNo, String country) {
+        this.name = name;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.country = country;
+    }
+
     public String getName() {
         return name;
     }
